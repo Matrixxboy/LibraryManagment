2644fb9463d9a919aba188188abad041d83612eb
Utsav Lankapati <168621377+Matrixxboy@users.noreply.github.com>
2025-02-10 16:20:22 +0530

    Update connection.js

--- lms-backend/connection/connection.js
+++ lms-backend/connection/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const userName = 'matrixboy' //user name of the Database user
-const dbPassword = 'utsav1472004' //password of Database user
+const dbPassword = '/' //password of Database user
 const connectionString = `mongodb+srv://${userName}:${dbPassword}@librarymanagment.yrw7k.mongodb.net/`
 
 //main connection to the MongoDB 
