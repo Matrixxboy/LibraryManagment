6fbe812f84c8dd26829fbf12d5082c8dbc6a6380
Utsav Lankapati <matrix.utsav.lanakapti@gmail.com>
2025-02-11 18:17:31 +0530

    Add .env file for more security.

--- lms-backend/connection/connection.js
+++ lms-backend/connection/connection.js
@@ -1,12 +1,19 @@
+require('dotenv').config();
 const mongoose = require("mongoose");
-const userName = 'matrixboy' //user name of the Database user
-const dbPassword = 'utsav1472004' //password of Database user
+const userName = process.env.MONGOOSE_USERNAME;
+const dbPassword = process.env.MDB_PASSWORD;
 const connectionString = `mongodb+srv://${userName}:${dbPassword}@librarymanagment.yrw7k.mongodb.net/`
 
-//main connection to the MongoDB 
+// To Check database connection issues.
+if (!userName || !dbPassword) {
+    console.error("Error: MONGOOSE_USERNAME or MDB_PASSWORD environment variables are not set.");
+    process.exit(1); // Exit the process if credentials are missing
+}
+
+//main connection to the MongoDB
 mongoose.connect(connectionString).then(()=>{
     console.log("Database Connected");
 }).catch(()=>{
-    console.log("Something is wrong")
+    console.log("Something is wrong with the Database connection");
 })
 
