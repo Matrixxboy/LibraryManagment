acf188598e50ff0973d7111453da26496d8bdaeb
Utsav Lankapati <matrix.utsav.lanakapti@gmail.com>
2025-02-11 18:37:06 +0530

    update

--- lms-backend/connection/connection.js
+++ lms-backend/connection/connection.js
@@ -1,12 +1,18 @@
+require("dotenv").config()
 const mongoose = require("mongoose");
-const userName = 'matrixboy' //user name of the Database user
-const dbPassword = 'utsav1472004' //password of Database user
+const userName = process.env.MONGODB_USERNAME;
+const dbPassword = process.env.MONGODB_PASSWORD;
 const connectionString = `mongodb+srv://${userName}:${dbPassword}@librarymanagment.yrw7k.mongodb.net/`
+
+if(!userName || !dbPassword){
+    console.error("MongoDB connection error with the username and password");
+    process.exit(1);
+}
 
 //main connection to the MongoDB 
 mongoose.connect(connectionString).then(()=>{
     console.log("Database Connected");
 }).catch(()=>{
-    console.log("Something is wrong")
+    console.log("Something is wrong connecting database...");
 })
 
