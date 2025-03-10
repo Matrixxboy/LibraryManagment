edeb478eeca28424d8c4a383b94a9bcfa4c14a4c
Matrixxboy <matrix.utsav.lankapati@gmail.com>
2025-03-10 17:08:09 +0530

    Navbar update

--- lms-frontend/frontend/src/Components/LayoutComponents/navbar/Navbar.jsx
+++ lms-frontend/frontend/src/Components/LayoutComponents/navbar/Navbar.jsx
@@ -1,39 +1,33 @@
-import React from 'react';
-import {Link} from "react-router-dom";
-import ("./navbar.css");
-//this is update check
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import "./navbar.css";
+
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <div>
-            <nav className="navbar navbar-expand-lg ">
-                <div className="container-fluid bg-dark ">
-                    <Link className="navbar-brand text-white" to="/">Book Store</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                            <Link className="nav-item nav-link active text-white" to="/">
-                                Home
-                            </Link>
-                            <Link className="nav-item nav-link active text-white" to="/Profile">
-                                Profile
-                            </Link>
-                            <Link className="nav-item nav-link text-white" to="/Book">
-                                Books
-                            </Link>
-                            <Link className="nav-item nav-link text-white" to="/Addbooks">
-                                Add Book
-                            </Link>
-                            <Link className="nav-item nav-link text-white" to="/Signup">
-                                Login
-                            </Link>
-                        </ul>
-                    </div>
+        <nav className="navbar navbar-expand-lg bg-dark">
+            <div className="container-fluid">
+                <Link className="navbar-brand text-white" to="/">Book Store</Link>
+                <button
+                    className="navbar-toggler visible-toggler"
+                    type="button"
+                    onClick={() => setIsOpen(!isOpen)}
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li className="nav-item"><Link className="nav-link text-white" to="/">Home</Link></li>
+                        <li className="nav-item"><Link className="nav-link text-white" to="/Profile">Profile</Link></li>
+                        <li className="nav-item"><Link className="nav-link text-white" to="/Book">Books</Link></li>
+                        <li className="nav-item"><Link className="nav-link text-white" to="/Addbooks">Add Book</Link></li>
+                        <li className="nav-item"><Link className="nav-link text-white" to="/Signup">Login</Link></li>
+                    </ul>
                 </div>
-            </nav>
-        </div>
-    )
+            </div>
+        </nav>
+    );
 }
 
-export default Navbar
+export default Navbar;
